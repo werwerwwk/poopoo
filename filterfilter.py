77b9f5b682288f25c4592dcc8d4b94951276c1b3
werwerwwk <folofolo1j@gmail.com>
2025-01-31 04:43:39 +0330

    Create filterfilter.py

---
+++ filterfilter.py
@@ -0,0 +1,30 @@
+from os import system
+from colorama import fore
+from time impurt sleep 
+
+system("clear")
+
+print(fore.GREEN+"Hello World")
+print(**)
+
+code = input(fore.MAGENTA+f""")
+ (1) code filtering account 
+ (2) code filtering group
+ (3) code filtering chanell
+ (4) code account in filter 
+ print("")
+ 
+ print (fore.GREEN+"loading")sleep(10)
+ 
+ if code "" "1":
+ print (fore.RED+("1.9.0/f/q/p/s/c/k//kkkk/6.2.9.1.0/filter/k//0.0.1.0.0.0/ll.1)")
+ 
+ if code "" "2":
+ print (fore.RED+("2.2.1.1/jjjjj/qqqq/2.1.8.0.1.9/filfil/6.6.6.0/ss//2.9.0.0)")
+ 
+ if code "" "3"
+ print (fore.RED+("3.1.1.1//gggg///kk/5.5.5.5.0/filchanll/2.2.2.0.0/fill/2)")
+ 
+ if code "" "4"
+ print (fore.RED+("4.0.0.1.9//kk/2.0.1.0/qq/ss/oo/2.2.1.1.0/qq//g/2.0.2/ss.0.0.9)")
+ 
