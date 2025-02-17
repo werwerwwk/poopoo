aab9e5b33bbcf19e1cd63b86c628797535956d58
peepee <folofolo1j@gmail.com>
2025-02-17 17:56:00 +0330

    Update rubikafiltering.py

---
+++ rubikafiltering.py
@@ -0,0 +1,328 @@
+# maxtor
+# Khasteh Az Hameh
+print("")
+print("")
+print(" ")
+print("""
+ ▄▄▄  ▄▄▄     ▄▄     ▄▄▄  ▄▄▄
+ ███  ███    ████     ██▄▄██
+ ████████    ████      ████
+ ██ ██ ██   ██  ██      ██
+ ██ ▀▀ ██   ██████     ████
+ ██    ██  ▄██  ██▄   ██  ██
+ ▀▀    ▀▀  ▀▀    ▀▀  ▀▀▀  ▀▀▀""")
+print(" ")
+print("""
+ ▄▄▄▄▄▄▄▄    ▄▄▄     ▄▄▄▄▄▄
+ ▀▀▀██▀▀▀  ██▀▀██   █▀▀▀▀██
+    ██     ██    ██  ██    ██
+    ██     ██    ██  ███████
+    ██     ██    ██  ██  ▀██▄
+    ██      ██▄▄██   ██    ██
+    ▀▀       ▀▀▀▀    ▀▀    ▀▀▀ """)
+print (" ")
+print("Load . . .")                 
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("      GOD FILTERING")
+print (Fore.YELLOW + "")
+x = """ Loading ~ ~ ~ 
+
+
+
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⣶⣶⣶⣶⣤⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠉⠉⠉⠉⠉⠙⢢⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢘⡉⢻⡇⠀⠀⠀⠀⠀⠀⠀⠀⢐⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠳⡦⣸⣷⣄⡀⢀⣀⡀⠀⠀⠀⡂⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡊⣿⣿⣿⣿⣯⡩⣉⠹⢷⢦⠁⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢰⠀⢻⣿⣿⣿⣿⣿⣿⣶⣶⡞⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡌⠀⠀⢀⠉⠻⣿⣿⣿⣿⣿⡇⠀⠀⠀
+⠀⣀⠠⠤⠐⠚⠱⠀⠀⠀⠈⠀⠀⠀⠉⣻⠛⠋⠀⠀⠀⠀
+⡇⠀⠀⡊⠠⠐⠁⠀⠀⠀⠀⢰⠀⢀⠆⠿⡀⠀⠀⠀⠀⠀
+⡗⠒⠒⠀⠀⠀⠠⠤⢤⡀⠀⢸⠀⠘⠀⠀⢌⠑⢢⠀⠀⠀
+⡇⠠⠀⠀⠀⠀⠀⠀⠀⠀⠑⢺⠀⠐⠀⠀⠂⠀⠀⠉⠒⠢⣄ 
+⡇⠀⠀⠀⠀⠀⠀⠂⢰⠤⠀⠀⢦⠁⠀⠀⢂⠀⠀⠐⠄⠀⠠⠈⠉⠑⠦⡀
+⡇⠀⡀⢀⠐⡀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠐⠀⠀⠀⠀⠀⠀⠀⠁⠄⠀⠀⠘
+
+
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+print(f"{red} ")
+d = ("""
+
+ _ __ ___   __ ___  _| |_ ___  _ __ 
+ | '_ ` _ \ / _` \ \/ / __/ _ \| '__|
+ | | | | | | (_| |>  <| || (_) | |   
+ |_| |_| |_|\__,_/_/\_\\__\___/|_|
+
+                            maxtor
+         GOOD FILTERING RUBIKA 
+
+                    CODE FILTERI RUBIKA
+            
+     
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣇ 
+⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⡿⠋
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⠟⠻⣧
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣄⠀
+⠀⠸⢶⣤⣤⣤⡤⢶⣺⣵⣮⣗⡶⢤⣤⣤⣤⡶⠇
+⢼⣿⣶⣶⣶⣾⣿⣿⠋⣭⣭⠙⣿⣿⣷⣶⣶⣶⣿⡷
+⠀⣿⣿⣿⣿⣿⣿⣿⣷⣯⡿⣾⣿⣿⣿⣿⣿⣿⣿
+⠀⣿⣿⣿⡆⡨⡯⣮⢻⢷⣶⣟⣵⢽⣅⢰⣿⣿⣿
+⠀⢿⣿⣿⣿⡶⢲⣏⣿⢰⡆⣿⣻⣆⣶⣿⣿⣿⣿
+⠀⢹⣿⣿⣿⣶⣶⣿⣷⣶⣶⣾⣿⣶⣶⣿⣿⣿⠇
+⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏
+⠀⠀⠀⠈⠻⣿⣿⣿⣿⣉⣙⣯⣏⣿⣿⠟⠀
+⠀⠀⠀⠀⠀⠢⢝⣻⠶⣶⣿⠶⣟⡫⠔
+⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⠶⠛⠉ """)
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+# Chaker * _ *  Hal Kardi?
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("Play - - - ! ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                       RUBIKA ID - @CYROSIF IM MAXTOR")
+print("                      RUBIKA= On -- @CYROSIF")
+print ("                telegram @HACKER_MAXTOR")
+print ("insta @creator_tor")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("!!")
+time.sleep(0.7)
+print ("lod....")
+time.sleep(0.7)
+print ("n/y")
+time.sleep(0.6)
+print ("....")
+time.sleep(0.5)
+print ("......")
+time.sleep(0.4)
+print (".........")
+time.sleep(0.3)
+print ("..................")
+time.sleep(0.2)
+print (".........................")
+time.sleep(0.1)
+print (".............................")
+time.sleep(0.5)
+print ("...............................")
+time.sleep(0.1)
+print (".................................")
+time.sleep(0.5)
+print ("....................................")
+time.sleep(0.1)
+print ("~iegn")
+time.sleep(0.5)
+print ("cood info server")
+time.sleep(0.1)
+print ("script server cood")
+time.sleep(0.5)
+print ("server1:y//d/f")
+time.sleep(1.0)
+print ("server2:f//d/")
+time.sleep(0.5)
+print ("server3:g/d//")
+time.sleep(0.1)
+print ("server4:d//f/h")
+time.sleep(0.5)
+print ("server5:f//a/y")
+time.sleep(1.0)
+print ("making script maxtor")
+time.sleep(0.5)
+print ("script making cood filter rubika.apk 2.9.5 80%")
+time.sleep(1.0)
+print ("script server")
+time.sleep(0.5)
+print ("script connect server rubika.ir/rubika.com/ file os server")
+time.sleep(1.0)
+print ("cood filter group 90% apn cood proxy-server")
+time.sleep(0.5)
+print ("cood filter account rubika 87% apn cood filter proxy-port:8000")
+time.sleep(1.0)
+print ("cood filter chaneel rubika 78% apn proxy file port:auth")
+time.sleep(0.5)
+print ("")
+print (" ")
+print ("")
+# 1 chanel 2 Group 3 acc
+up = input(""" 
+
+[cood filter channel rubika (1) ]
+[cood filtr group rubika    (2) ]
+                 
+[cood filter Account rubika (3) ]
+                  
+[Super   (0) ]
+
+[Exit    (4) ]
+
+[report aut (5) ]
+
+[Code Rp (6) ]
+
+[ account on filter (7) ]
+
+Which do you want>>>""")
+print("")
+print(f"{pink} ")
+time.sleep(3.0)
+print(""" in the name of God 
+_____________
+
+Loading . . . .
+
+. . . . . . .  . .
+
+ . . . . . . . . . . 
+ 
+💣 💣 💣 💣 💣 💣 💣 💣
+
+                    
+    
+|||||Telegram: T.me/hackgm ||||
+|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|
+____________ id:hacker_maxtor___________
+""")
+print(f"{red} ")
+
+if up == "7":
+    os.system("python sms.py")
+if up == "6":
+    os.system("python report.py")
+if up == "5":
+    os.system("python pass.py")
+if up == "4":
+    os.system("exit 5")
+
+if up == "0":
+         print ('(Super) Code filter =>' + '%s%s  (5.5.0.0.supsup/filterinng/rubi/rubi//0.0.1.1/w//514.80//s/s/s/pwpw/plpl/lflf/i/i/k/k/v/s/c/q/q/q/j/ee/e/e/o/o/w/w/w/k/wdsx/jj/j/k/p/r/e/r/r/rk/wws/o/ll/vx/x/x/x/x/eq/eq/i/o/p/w/o/ye/x/i/i/s/s/vv/vv/o/3.3.6.1) %s' % (fg('red'), bg('black'), attr('reset')))
+
+
+if up == "1":
+        print ('code Fil Channel ≠>' + '%s%s (8.8.7.0/e/sssjb///q//2060.80/w/3/jwjn/idhd/jsb/k/ff/w/i/i/o/xx/0.0.0.0/k/lp/s/d/f/f/v/k/s/o/k/o/w/o/i/i/rrr/ooo/rew/opw/o/w/w/xj/nsb/jsn/jndj/k/k/p/p/p/ow/qj/vv/yredbko/ood/3.0.4.0.6.5) %s' % (fg('red'), bg('yellow'), attr('reset')))
+0
+
+if up != "7":
+        print('code account on filter ≠>' + '%s%s (4.0.4.0/ac//i/n/f/i/l/e/k/l/s/o/p/pq/p/p/p/w/w/w/w/w/wp/o/s//oo/ow/wa/js/4.4.0.4.0.3/ks//o/s/s/jsns/ksks/ksks/wjwk/kskks/kskks/ksksk/kskkk/kskkk//kskks/9.9/jsj/ksos/skkk/ksks/oskw/oo/psop/5.4.1.0.8)
+
+
+if up == "2":
+        print ('code group ≠>' + '%s%s (0.0.4.1/q.s.qfssvvw/powp/8.2.1//0.89.1.4.5.6/jkhn/pe/rbiw.2.0.7.8.0/j/k/w/o/q/l/w/r/y/s/p/rewqj//bioks//xjsn/5.5.0.1.3/bsoj/ownn/owjw/33.0/sjnsb/boo/wbbsi/jsjsn/wso/wor/jjk/jdn/10/iwjnei/eihbeiw/is/10) %s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "6":
+
+        print('cod rp ≠>' + '%s%s
+(7.0.7.0/cod/rp/r/r/p/r/j/s/k/k/s/osjs/ksk/ksks/ksks/ksks/ksks/6.6.6.6.6.5.5.4/js/ksk/kks/j/j/j/je/o/l/i/ow/o/o/e/ow/k/d/o/o/o/w/w/w/l/l/l/l/l/ljn/jjs/kks/jsnjs/jsjs/6.9./oo/ks/jjs/7.7.2)
+
+if up == "3":
+
+        print ('code Filter account ≠>' + '%s%s (9.1.3.4.0//gigig/6.9.2.5/jjkjjn/qfswi/lqpe/4.3.9.5.7/wrrr//g/3.6.7/vvv.8.8.1.5/rrr//pp/s/yy/4/rrrr/yiw/w/3/jk//wqwgy/sqwq/99/sbhj/jshi/wbsj/jsbbw/nwi/oow/wjjsn/sxoow/jdieije/ooo/iwksn/iiiwik/woosj/90) %s' % (fg('red'), bg('blue'), attr('reset')))
+
+if up != "3":
+        print("""
+        
+             Khalegh tor""")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""maxtor""")
+print (" group rubikam =>> https://rubika.ir/joinc/JDDGCJF0YUTLWSRZLYDCJWIOBMCANFKJ ")
+print(f"{blue}" )
+print(" ")
+print("love hack")
+print("")
+print(" im maxtor making hackgm")
+print("")
+print("ip rubika.ir = 5.106.10.226 ")
+print("")
+print(" Chanel Telegram=>> @HACKGM -")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Ostoreh Filtering-hacker All maxtor")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('Cod Ro Dorost Bezan 70% Filter Mikone  ' + '%s%s TanK You My Friend  %s'  % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print (""" {code ro ( 5 ) bar bezanin to Sayer!!}
+baraye account Tanks""")
+print(" ")
+print(" ")
+print(" ")
+print ("          THE END!")
+print("")
+print(""" (Update Kardan script) 👇:
+
+rm -rf filter-rubika
+rm -rf filter.py
+
+
+git clone https://github.com/werwerwwk/poopoo
+
+
+chmod +x Filter.py
+
+
+cd peepee
+
+
+python rubikafiltering.py
+
+""")
+print("")
+print("")
+print("         </maxtor>")
+print ("")
+time.sleep(0.3)
+print(f"{red}" )
+time.sleep(1)
+print ("""ostorh hack""")
+print("")
+print (Fore.WHITE + "")
